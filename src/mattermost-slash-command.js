773a7c60fc0ac6b07b097ca8b9de35a7293ee369
eichisanden <eichisanden@gmail.com>
2018-04-08 00:14:04 +0900

    modify message

--- src/mattermost-slash-command.js
+++ src/mattermost-slash-command.js
@@ -12,16 +12,19 @@
 
 const TextMessage = require('hubot').TextMessage;
 
+
 module.exports = (robot) => {
   robot.router.post('/hubot-mattermost-slash-command', (req, res) => {
     const user = robot.brain.userForId(req.body.user_id);
     user.name = req.body.user_name;
     user.room = req.body.channel_name;
-    robot.receive(new TextMessage(user, req.body.text));
+    robot.receive(new TextMessage(user, `${robot.name}` ${req.body.text}`));
 
     const msg = {};
     msg.response_type = "in_channel";
-    msg.text = req.body.user_name + " orders `" + req.body.command + " " + req.body.text + "`";
+    const message = `Okey,${req.body.user_name} orders \`${req.body.command} ${req.body.text}\`}`;
+    msg.text = message;
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).send(msg);
     res.end();
   });
